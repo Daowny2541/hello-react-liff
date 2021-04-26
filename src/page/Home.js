f438aeed3cf868b38e675341d99b99fc99277586
Daowny Rattana <rattana.rittawat@gmail.com>
2021-04-26 17:32:36 +0700

    update form home

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,18 +1,46 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Col, FormText } from 'reactstrap';
 
 const Home = (props) => {
     return (
-<Form >
-<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-  <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-  <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
-</FormGroup>
-<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-  <Label for="examplePassword" className="mr-sm-2">Password</Label>
-  <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
-</FormGroup>
-<Button>Submit</Button>
+<Form>
+  <Col md-6>
+    <FormGroup tag="fieldset">
+      <legend>Register</legend>
+        <FormText color="muted">
+          Welcome to Staff pay slip This is the registerion page to access intermal services.<br />Please register to login.
+        </FormText>
+    </FormGroup>
+  </Col>
+  <Col md-16>
+    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+      <Label for="firstName" className="mr-sm-2">First name</Label>
+      <Input type="text" name="firstName" id="firstName" placeholder="Enter first name" />
+    </FormGroup>
+  </Col>
+  <Col md-16>
+    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+      <Label for="lastName" className="mr-sm-2">Last name</Label>
+      <Input type="text" name="lastName" id="lastName" placeholder="Enter last name" />
+    </FormGroup>
+  </Col>
+  <Col md-16>
+    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+      <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+      <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+    </FormGroup>
+  </Col>
+  <Col md-16>
+    <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+      <Label for="examplePassword" className="mr-sm-2">Password</Label>
+      <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
+    </FormGroup>
+  </Col>
+  <br/>
+  <Col sm-16>
+    <Button outline color="primary">Register</Button>
+  </Col>
+  
 </Form>
         
     );
